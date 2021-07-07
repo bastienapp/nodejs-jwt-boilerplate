cffe1132677edd889c713d0196e18d6ae812a11d
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-07-07 11:29:48 +0200

    fixes

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const cors = require('cors');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const base64 = require('base-64');
 require('dotenv').config();
 const connection = require('./database');
 
@@ -17,6 +20,120 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Your code here!
+app.post('/register', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please specify both email and password' });
+  } else {
+    const hash = bcrypt.hashSync(password, 10);
+    connection.query(
+      `INSERT INTO user(email, password) VALUES (?, ?)`,
+      [email, hash],
+      (error, result) => {
+        if (error) {
+          res.status(500).json({ errorMessage: error.message });
+        } else {
+          res.status(201).json({
+            id: result.insertId,
+            email,
+            password: 'hidden',
+          });
+        }
+      }
+    );
+  }
+});
+
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res
+      .status(400)
+      .json({ errorMessage: 'Please specify both email and password' });
+  } else {
+    connection.query(
+      `SELECT * FROM user WHERE email=?`,
+      [email],
+      (error, result) => {
+        if (error) {
+          res.status(500).json({ errorMessage: error.message });
+        } else if (result.length === 0) {
+          res.status(403).json({ errorMessage: 'Invalid email' });
+        } else if (bcrypt.compareSync(password, result[0].password)) {
+          // Passwords match
+          const user = {
+            id: result[0].id,
+            email,
+            shop: result[0].shop,
+            password: 'hidden',
+          };
+          const token = jwt.sign(
+            {
+              id: user.id,
+              email: user.email,
+              shop: user.shop,
+              admin: user.shop === 'admin',
+            },
+            JWT_AUTH_SECRET,
+            {
+              expiresIn: 60 * 60 * 24 * 15,
+            }
+          );
+          res.status(200).json({ user, token });
+        } else {
+          // Passwords don't match
+          res.status(403).json({ errorMessage: 'Invalid password' });
+        }
+      }
+    );
+  }
+});
+
+const authenticateWithJsonWebToken = (req, res, next) => {
+  if (req.headers.authorization !== undefined) {
+    const token = req.headers.authorization.split(' ')[1];
+    jwt.verify(token, JWT_AUTH_SECRET, (err) => {
+      if (err) {
+        res
+          .status(401)
+          .json({ errorMessage: "you're not allowed to access these data" });
+      } else {
+        const tokenDecoded = JSON.parse(base64.decode(token.split('.')[1]));
+        console.log('tokenDecoded', tokenDecoded);
+        req.admin = tokenDecoded.admin;
+        next();
+      }
+    });
+  } else {
+    res
+      .status(401)
+      .json({ errorMessage: "you're not allowed to access these data" });
+  }
+};
+
+const isAdmin = (req, res, next) => {
+  if (req.admin) {
+    next();
+  } else {
+    res.status(401).json({ errorMessage: 'not admin' });
+  }
+};
+
+app.get('/users', authenticateWithJsonWebToken, isAdmin, (req, res) => {
+  connection.query(`SELECT * FROM user`, (error, result) => {
+    if (error) {
+      res.status(500).json({ errorMessage: error.message });
+    } else {
+      res.status(200).json(
+        result.map((user) => {
+          return { ...user, password: 'hidden' };
+        })
+      );
+    }
+  });
+});
 
 // Don't write anything below this line!
 app.listen(SERVER_PORT, () => {
